65673422fe13299c83b28b1969f2707c6453396d
laurenttainturier <laurent.tainturier.pro@gmail.com>
2020-07-09 15:39:24 +0200

    Initial Commit

---
+++ AR Experiment - iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AR Experiment - iOS
+//
+//  Created by Laurent Tainturier on 09/07/2020.
+//  Copyright © 2020 Laurent Tainturier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
